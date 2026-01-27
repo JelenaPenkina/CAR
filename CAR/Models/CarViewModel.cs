b84e56ae90ba2016d8928b4cab0701d13a8f0364
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2026-01-27 19:22:40 +0200

    Create CarViewModal

---
+++ CAR/Models/CarViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CAR.Models
+{
+    public class CarViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Make is required")]
+        [StringLength(50)]
+        public string? Make { get; set; }
+
+        [Required(ErrorMessage = "Model is required")]
+        [StringLength(50)]
+        public string? Model { get; set; }
+
+        [Required(ErrorMessage = "Year is required")]
+        [Range(1886, 2026, ErrorMessage = "Year must be between 1886 (first car) and 2026 (current year)")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Color is required")]
+        [StringLength(30)]
+        public string? Color { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0, 1000000, ErrorMessage = "Price must be positive")]
+        public int Price { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
