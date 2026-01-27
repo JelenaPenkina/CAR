f3ff198c962aa9185298ef498544c1f721e43b58
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2026-01-27 19:06:32 +0200

    Fix git index, ignore Visual Studio files

--- CAR/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CAR.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
